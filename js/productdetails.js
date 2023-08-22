176b3514c65a5b14846828f56d3c2a5002ea695e
EF-Pami <pamijoelpami@gmail.com>
2023-08-22 19:47:30 +0200

    update

--- js/productdetails.js
+++ js/productdetails.js
@@ -6,11 +6,11 @@ const id = params.get('id');
 
 const WOO_API_KEY ='ck_3652f8cb73dec06049c63fd04a29fbb32e837c8b'
 const WOO_API_SECRET ='cs_aaa7268a9a0546bf8bb5e89935e011ee68643b9b'
-const BASE_URL = `https://localhost/flower-power/wp-json/wc/v2/products?consumer_key=${WOO_API_KEY}&consumer_secret=${WOO_API_SECRET}`
-const product_detail = BASE_URL + id;
+const BASE_URL = `https://localhost/flower-power/wp-json/wc/v2/products/${id}?consumer_key=${WOO_API_KEY}&consumer_secret=${WOO_API_SECRET}`
+//const product_detail = BASE_URL + id;
 
 async function fetchdata() {
-    const response = await fetch(product_detail);
+    const response = await fetch(BASE_URL);
     const data = await response.json();
     //console.log(data)
     return data;
@@ -18,20 +18,21 @@ async function fetchdata() {
 fetchdata();
 
 async function renderHTml() {
-    const data = await fetchdata();
+    const products = await fetchdata();
     const mainElement = document.querySelector(".flowerdetails");
     mainElement.innerHTML = `
-    <div class="details">    
-        <h3> ${data.name}</h3>
-        <p> ${data.description}</p>
-        <p> ${data.Price}</p>
-        <p> ${data.regular_price}</p>   
+    <div id="details">    
+        <h3> ${products.name}</h3>
+        <p> ${products.description}</p>
+        <p> ${products.Price}</p>
+        <p> ${products.regular_price}</p>   
     </div>
     <div>
-        <img src ="${data.permalink?.src}" alt="">
+        <img src ="${products.images[0].src}" alt="${products.name}"/>
     </div>    
     `
     document.title = 'product_details';
+    //console.log(products);
 
 }
 
