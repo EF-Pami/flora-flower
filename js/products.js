c20b3b8286b036d06e16e48c162d4297feeb0690
EF-Pami <pamijoelpami@gmail.com>
2023-08-26 00:01:32 +0200

    Update products.js

--- js/products.js
+++ js/products.js
@@ -6,31 +6,51 @@ const flowers_block = document.querySelector(".flowers-block")
 const BASE_URL = 'https://cors.noroff.dev/pami.no/wp-json/wc/store/products?'
 async function fetchdata() {
     try {
-        console.log (BASE_URL)
+        //console.log (BASE_URL)
         const response = await fetch(BASE_URL);
-    const data = await response.json();
-    return data;
+        const data = await response.json();
+        return data;
 
-    } catch (error) {
+        } catch (error) {
         //console.error(error);
     }
     
 }
 
+/*async function renderproductsThumbnails() {
+    const productcontainer = document.getElementsByClassName(flowers_block);
+    const products = await fetchdata();
+    products.forEach((product) =>{
+        const thumbnail = document.createElement("div");
+        thumbnail.classList.add("product-thumbnail");
+
+        const image = document.createElement("img");
+        image.src = product.immageUrl;
+        image.alt = product.name;
+
+        const productName = document.createElement("p");
+        productName.textContent = product.Name;
+
+        thumbnail.appendChild(image);
+        thumbnail.appendChild(productName);
+        productcontainer.appendChild(thumbnail);
+    });
+}
+
+window.onload = renderproductsThumbnails;*/
 
 async function renderHTml() {
     const products = await fetchdata();
-    //const featuredProducts = products.filter(products => products.featured);
+    const featuredProducts = products.filter(products => products.featured);
     //console.log({products})
     //console.log({html: flowers_block.innerHTML})
     flowers_block.innerHTML = ``;
     products.forEach(function (products, index) {
         flowers_block.innerHTML += `
         <a href="productdetails.html?id=${products.id}"><img src="${products.images[0].src}" alt="${products.name}"/></a>
-        
         `
     })
 }
-
+        
 renderHTml()
 
