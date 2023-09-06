f24e5f39d4cfe81df023c4163a484a66e08b6895
EF-Pami <pamijoelpami@gmail.com>
2023-09-06 23:09:21 +0200

    Update productdetails.js

--- js/productdetails.js
+++ js/productdetails.js
@@ -3,7 +3,7 @@ const querystring = document.location.search;
 console.log({querystring});
 const params = new URLSearchParams(querystring);
 const id = params.get('id');
-const productPrices = document.createElement("p");
+
 //const WOO_API_KEY ='ck_3652f8cb73dec06049c63fd04a29fbb32e837c8b'
 //const WOO_API_SECRET ='cs_aaa7268a9a0546bf8bb5e89935e011ee68643b9b'
 //const BASE_URL = `https://localhost/flower-power/wp-json/wc/v2/products/${id}?consumer_key=${WOO_API_KEY}&consumer_secret=${WOO_API_SECRET}`
